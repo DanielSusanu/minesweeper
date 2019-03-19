44a5b531c7cab8ccc590d44d9e23912b06d5806f
Daniel Susanu <d.susanu.development@gmail.com>
2019-03-19 01:30:30 +0000

    Add most of the game functionalities

--- sketch.js
+++ sketch.js
@@ -1,7 +1,87 @@
+var grid;
+var cols;
+var rows;
+var w = 40;
+var canvasW= 401;
+var totalBees=20;
+
 function setup() {
-    createCanvas(400, 400);
-  }
+    createCanvas(canvasW, canvasW);    
+    cols = floor(width / w);
+    rows = floor(height / w);
+    grid = make2DArray(cols,rows);
+
+    // create neighbors
+    for(var i=0; i < cols; i++){
+        for(var j=0; j < rows; j++){
+            grid[i][j] = new Cell(i, j, w);
+        }
+    }
+
+    // Pik totalBees spots
+    var options= [];   
+    
+    for(var i=0; i < cols; i++){
+        for(var j=0; j < rows; j++){
+            options.push([i, j]);
+        }
+    }
+
+    for (var n=0; n < totalBees; n++){
+        var index = floor(random(options.length));
+        var choice = options[index];
+        var i = choice[0];
+        var j = choice[1];
+        options.splice(index, 1);
+        grid[i][j].bee = true;
+    }
+
+     
+    // Count neighbors
+    for(var i=0; i < cols; i++){
+        for(var j=0; j < rows; j++){
+            grid[i][j].countNeighbors();
+        }
+    }
+}
   
-  function draw() {
-    background(220);
-  }
+function draw() {
+    background(255);
+    for(var i=0; i < cols; i++){
+        for(var j=0; j < rows; j++){
+            grid[i][j].show();
+        }
+    }
+
+}
+
+function gameOver(){
+    for(var i=0; i < cols; i++){
+        for(var j=0; j < rows; j++){
+            grid[i][j].revealed = true;
+        }
+    }
+}
+
+function mousePressed(){
+    for(var i=0; i < cols; i++){
+        for(var j=0; j < rows; j++){
+            if(grid[i][j].contains(mouseX, mouseY)){
+                grid[i][j].reveal();
+
+                if(grid[i][j].bee){
+                    gameOver();
+                }
+                return;
+            }
+        }
+    }
+}
+
+function make2DArray(cols, rows){
+    var arr = new Array(cols);
+    for (var i=0; i < arr.length; i++){
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
